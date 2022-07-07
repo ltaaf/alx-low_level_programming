1b435a55876307d88c66cae508738c10e22a152f
AFO ABDEL FADIL <45314779+ltaaf@users.noreply.github.com>
2022-07-07 22:43:24 +0000

    correction

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,30 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int main(int argc, char *argv[]) {
+//	======================================debut=======================
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
 int largest_number(int a, int b, int c)
 {
-int largest;
+    int largest;
 
-if (a > b && a > c)
-{
-	largest = a;
-}
-else if (b > c && b > a)
-{
-	largest = b;
-}
-else if (c > b)
-{
-	largest = c;
-}
-else
-{
-	largest = b;
-}
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
 
-return (largest);
+    return (largest);
 }
 
-
+//	===================================fin debut===================
+	return 0;
 }
